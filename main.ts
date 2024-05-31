f4a5bff633646f1acc36f03d8da511cf784f469d
gitmalaika <malaikaimranimran2@gmail.com>
2024-05-31 06:25:52 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,78 @@
+// Exercises Q:2
+
+// let personName: String = "Malaika Imran";
+// console.log("Hello" , PersonName ,"would you like to learn some Python today?")
+
+
+// Exercises Q:3
+
+// let personName = "Malaika Imran";
+
+// console.log(personName.toLowerCase());
+
+// console.log(personName.toUpperCase());
+
+// console.log(personName.replace(/\b\w/g, (word) => word.toUpperCase()));
+
+// Exercises Q:4
+
+// console.log('Albert Einstein once said, “A person who never made a mistake never tried anything new"');
+
+// Exercises Q:5
+
+// let famousperson : strig = "Albert Einstein";
+
+// let message :string  ="once said, “A person who never made a mistake never tried anything new.”"
+
+// console.log(famousperson,message)
+
+// Exercises Q:6
+
+// let whitespaceName : string = "\n\t MalaikaImran\n\t";
+// console.log(whitespaceName);
+
+// let withoutwhitespaceName : string =whitespaceName.trim();
+// console.log(withoutwhitespaceName); 
+
+// Exercises Q:7 and 8
+
+// const addition : number = 4+4 ;
+// console.log(addition);
+
+// const subtraction : number = 10-2;
+// console.log(subtraction);
+
+// const multiplication : number = 4*2;
+// console.log(multiplication);
+
+// const division : number = 16/2;
+// console.log(division);
+
+// Exercises Q:9
+
+// let favoritenumber : number = 5;
+// let message : string = "my favorite number is";
+
+// console.log(favoritenumber , message)
+
+// Exercises Q:10
+
+//addition +
+
+// const addition : number = 4+4 ;
+// console.log(addition);
+
+//subtraction -
+
+// const subtraction : number = 10-2;
+// console.log(subtraction);
+
+//multiplication *
+
+// const multiplication : number = 4*2;
+// console.log(multiplication);
+
+//division /
+
+// const division : number = 16/2;
+// console.log(division);
